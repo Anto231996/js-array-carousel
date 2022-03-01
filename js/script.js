6e235a1a6d7a1f1833d2012abc7cf11d696a70b0
Anto231996 <tony2396@hotmail.it>
2022-03-01 18:45:12 +0100

    ..

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ carouselImg.src = "img/01.jpg";
 document.getElementById('carousel').appendChild(carouselImg);
 carouselImg.style.width = '100%';
 carouselImg.style.height = '500px';
+
+let carouselContent = '';
+
+for (let i = 0 ; i < images.length ; i++){
+    carouselContent += `
+    <div class="item">
+        <img src="${images[i]}" alt="Random picture">
+    </div>`
+}
+
+const carouselWrapper = document.querySelector('div.thumbnails');
+carouselWrapper.innerHTML += carouselContent;
