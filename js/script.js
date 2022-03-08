73926edd0f38fb0a58350ba46c7989a8ae28d2b3
Anto231996 <tony2396@hotmail.it>
2022-03-08 23:23:51 +0100

    correzione

--- js/script.js
+++ js/script.js
@@ -1,25 +1,100 @@
 const images = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
+  "img/01.jpg",
+  "img/02.jpg",
+  "img/03.jpg",
+  "img/04.jpg",
+  "img/05.jpg",
 ];
 
-let carouselImg = document.createElement("img");
-carouselImg.src = "img/01.jpg";
-document.getElementById('carousel').appendChild(carouselImg);
+const title = ["Svezia", "Svizzera", "Gran Bretagna", "Germania", "Paradise"];
 
-let carouselContent = '';
+const text = [
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+  "Lorem ipsum",
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
+  "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
+];
 
-for (let i = 0 ; i < images.length ; i++){
-    carouselContent += `
-    <div class="item">
-        <img src="${images[i]}" alt="">
-    </div>`
+let itemsContent = "";
+let thumbnailsContent = "";
+
+for (let i = 0; i < images.length; i++) {
+  itemsContent += `
+        <div class="item">
+            <img src="${images[i]}" alt="${title[i]} picture">
+            <div class="item-description px-3">
+                <h2>${title[i]}</h2>
+                <p>${text[i]}</p>
+            </div>
+        </div>`;
+
+  thumbnailsContent += `
+        <div class="thumbnail">
+            <img src="${images[i]}" alt="Thumbnail of ${title[i]} picture">
+        </div>`;
 }
 
-const carouselWrapper = document.querySelector('div#thumbnails');
-carouselWrapper.innerHTML += carouselContent;
+const itemsElement = document.querySelector("div.slider .items");
 
-const carouselElements = document.getElementsByClassName('item');
+itemsElement.innerHTML = itemsContent;
+
+const thumbnailsElement = document.querySelector("div.slider .thumbnails");
+
+thumbnailsElement.innerHTML += thumbnailsContent;
+
+let activeElement = 1;
+
+document.getElementsByClassName("item")[activeElement].classList.add("active");
+
+document
+  .getElementsByClassName("thumbnail")
+  [activeElement].classList.add("active");
+
+const prev = document.querySelector("div.prev");
+
+prev.addEventListener("click", function () {
+  document
+    .getElementsByClassName("item")
+    [activeElement].classList.remove("active");
+  document
+    .getElementsByClassName("thumbnail")
+    [activeElement].classList.remove("active");
+
+  if (activeElement === 0) {
+    activeElement = images.length - 1;
+  } else {
+    activeElement--;
+  }
+
+  document
+    .getElementsByClassName("item")
+    [activeElement].classList.add("active");
+  document
+    .getElementsByClassName("thumbnail")
+    [activeElement].classList.add("active");
+});
+
+const next = document.querySelector("div.next");
+
+next.addEventListener("click", function () {
+  document
+    .getElementsByClassName("item")
+    [activeElement].classList.remove("active");
+  document
+    .getElementsByClassName("thumbnail")
+    [activeElement].classList.remove("active");
+
+  if (activeElement === images.length - 1) {
+    activeElement = 0;
+  } else {
+    activeElement++;
+  }
+
+  document
+    .getElementsByClassName("item")
+    [activeElement].classList.add("active");
+  document
+    .getElementsByClassName("thumbnail")
+    [activeElement].classList.add("active");
+});
